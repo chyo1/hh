c93033d447b818525adb45c1840a5846a5974e32
chyo1 <hyowon3856@gmail.com>
2025-08-07 15:35:19 +0900

    test : 포인트 조회 시 발생할 수 있는 에러 제시

---
+++ src/test/java/io/hhplus/tdd/point/PointCheckTest.java
@@ -0,0 +1,19 @@
+package io.hhplus.tdd.point;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PointCheckTest {
+
+	@Test
+	void 유저가_존재하지_않음() {
+		// given
+
+		// when
+
+		// then
+	}
+
+
+}
