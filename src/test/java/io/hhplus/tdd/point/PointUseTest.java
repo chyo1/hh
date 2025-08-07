77501d96316fbe7978661944abf88a40fad95b6f
chyo1 <hyowon3856@gmail.com>
2025-08-07 15:40:42 +0900

    test : 포인트 사용 시 발생할 수 있는 에러 제시

---
+++ src/test/java/io/hhplus/tdd/point/PointUseTest.java
@@ -0,0 +1,36 @@
+package io.hhplus.tdd.point;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PointUseTest {
+
+	@Test
+	void 유저가_존재하지_않음() {
+		// given
+
+		// when
+
+		// then
+	}
+
+	@Test
+	void 사용하려는_포인트가_음수() {
+		// given
+
+		// when
+
+		// then
+	}
+
+	@Test
+	void 보유한_포인트가_부족함() {
+		// given
+
+		// when
+
+		// then
+	}
+
+}
